910cf72e318d9d62edf5639865ccc7d809c6c74a
Shefaul-22 <cse1805022brur@gmail.com>
2025-10-06 23:53:35 +0600

    Dynamic responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 
 const navigationData = [
@@ -12,10 +13,24 @@ const navigationData = [
 
 
 const Navbar = () => {
-    return (
-        <nav>
 
-            <ul className='flex'>
+    const [open,setOpen] = useState(false)
+
+    const links = navigationData.map(route => <li className='mr-10'><a href="/"> {route.name}</a></li>)
+
+    return (
+        <nav className='flex justify-between mt-3 mx-8'>
+
+            <span className='flex' onClick={() => setOpen(!open)}>
+                { open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>}
+
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+                
+                <h3 className='ml-3'>My Navbar</h3>
+            </span>
+            <ul className='md:flex hidden'>
                 {
                     navigationData.map(route => <Link key={route.id} route={route}></Link>)
                 }
@@ -34,6 +49,8 @@ const Navbar = () => {
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
+
+            <button>Sign In</button>
 
         </nav>
     );
