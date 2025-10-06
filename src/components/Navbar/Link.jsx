77fcf1c5bf01bb8d761a252a8c89ab786d426680
Shefaul-22 <cse1805022brur@gmail.com>
2025-10-06 22:29:37 +0600

    Vite+ react setup done with navbar

---
+++ src/components/Navbar/Link.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Link = ({route}) => {
+    const {name,path} = route;
+    return (
+        <div>
+            <li className='mr-10'>
+                <a href={path}>{name}</a>
+            </li>
+        </div>
+    );
+};
+
+export default Link;
